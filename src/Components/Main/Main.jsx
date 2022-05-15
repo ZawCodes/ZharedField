085c696548ae57612d63846a649d479bbb206f4c
ZawHtetAung-Zhared <zawhtetaung.zhared@gmail.com>
2022-05-15 21:11:32 +0630

    update home and add animations

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,26 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from './Header/Header';
 import './Main.css';
+import TagCloud from 'TagCloud';
+import AnimatedLetters from '../AnimatedLetters'
 
 const Main = () => {
-    const tab = '&emsp;&emsp;&emsp;&emsp;';
-  return (
-    <>
-        <Header />
-        <section className='main-section'>
-            <main className='main-container'>
-                <div className='left-div'>
-                    <h3><span>Hello</span>, and welcome!</h3>
-                    <p><span className='tab'></span>I'm a <span>Front end developer</span> as well as an <span>artist</span>. Creating art and responsive website is my passion.</p>
-                    <p className='more'>I wanna know more!</p>
-                </div>
-                <div className='right-div'>
-                    Space for 3d text
-                </div>
-            </main>
-        </section>
-    </>
-  )
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    const welcomeText = ['H', 'i',',', ' ',`I`,`'`, 'm',' ','Z', 'h', 'a', 'r', 'e', 'd'];
+
+    const texts = [
+        'Javascript', 'Typescript', 'Html', 'Css', 'Angularjs', 'Angular', 'Reactjs', 'Nodejs', 'Git', 'Figma', 'Bootstrap', 'SQL'
+    ];
+
+
+    const options = {
+        radius: 300,
+    };
+    useEffect(() => {
+        setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 2500)
+
+        if (document.getElementsByClassName('tagcloud').length == 1) {
+            TagCloud('.tagcloud', texts, options);
+        }
+    }, [])
+
+    return (
+        <>
+            <Header />
+            <section className='main-section'>
+                <main className='main-container'>
+                    <div className='left-div'>
+                        <h3>
+                            <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={welcomeText}
+                            idx={1}
+                            />
+                        </h3>
+                        <p><span className='tab'></span>I'm a <span>Front end developer </span>
+                            as well as an <span>artist</span>
+                            . I create arts and responsive websites for a living.</p>
+                        <p className='more'>I wanna know more!</p>
+                    </div>
+                    <div className='right-div tagcloud'>
+                    </div>
+                </main>
+            </section>
+        </>
+    )
 }
 
 export default Main
