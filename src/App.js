54a5621fc7b2777c16b5703b0846481558a99b48
ZawHtetAung-Zhared <104304039+ZawHtetAung-Zhared@users.noreply.github.com>
2023-07-24 08:53:46 +0000

    ga testing

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ import Skill from './Components/Skill/Skill';
 import Work from './Components/Work/Work';
 import Contact from './Components/Contact/Contact';
 import Project from './Components/Project/Project';
+import ReactGA from "react-ga4";
 
 function App() {
+  // ReactGA.initialize("G-H6LTXTTNLJ");
+  // ReactGA.send({ hitType: "pageview", page: "/", title: "Home" });
+
   return (
     <>
       <Main />
